00d172c072f2abd13bd65eb3b9d6d0bff28b5d1b
Marius <68054322+CoteleaMarius@users.noreply.github.com>
2025-05-02 15:56:45 +0300

    Initial Commit

---
+++ CameraCapture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CameraCapture
+//
+//  Created by Marius Cotelea on 02.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CameraCaptureTests/CameraCaptureTests.swift
@@ -0,0 +1,17 @@
+//
+//  CameraCaptureTests.swift
+//  CameraCaptureTests
+//
+//  Created by Marius Cotelea on 02.05.2025.
+//
+
+import Testing
+@testable import CameraCapture
+
+struct CameraCaptureTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
